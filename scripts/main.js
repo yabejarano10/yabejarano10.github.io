35f3561f64bb688ff45de2443ebf231ccca48c0f
YESID ALEXANDER BEJARANOCAMACHO <ya.bejarano10@sis.virtual.uniandes.edu.co>
2019-02-07 21:31:08 -0500

    final details

--- scripts/main.js
+++ scripts/main.js
@@ -1,52 +1,51 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
     // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
+    $("a").on('click', function (event) {
 
-        // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
+            // Prevent default anchor click behavior
+            event.preventDefault();
 
-        // Store hash
-        var hash = this.hash;
+            // Store hash
+            var hash = this.hash;
 
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 800, function(){
-
-            // Add hash (#) to URL when done scrolling (default click behavior)
-            window.location.hash = hash;
-        });
-        } // End if
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
     });
 
-    $("footer").on('click', function play(){
+    $("footer").on('click', function play() {
         var audio = document.getElementById("audio");
         audio.play();
-        $(".round-image.profile").attr("src","images/face.jpg");
+        $(".round-image.profile").attr("src", "images/face.jpg");
     });
 
-    var $element=$('.each-event, .title');
+    //Animation for timeline
+    var $element = $('.each-event, .title');
     var $window = $(window);
     $window.on('scroll resize', check_for_fade);
     $window.trigger('scroll');
-    function check_for_fade() { 
-    var window_height = $window.height();
-    
-    $.each($element, function (event) {
-        var $element = $(this);
-        var element_height = $element.outerHeight();
-        var element_offset = $element.offset().top;
-        space = window_height - (element_height + element_offset -$(window).scrollTop());
-        if (space < 60) {
-            $element.addClass("non-focus");
-        } else {
-            $element.removeClass("non-focus");
-        }
- 
-    });
-};
-    
-    });
+
+    function check_for_fade() {
+        var window_height = $window.height();
+
+        $.each($element, function (event) {
+            var $element = $(this);
+            var element_height = $element.outerHeight();
+            var element_offset = $element.offset().top;
+            space = window_height - (element_height + element_offset - $(window).scrollTop());
+            if (space < 60) {
+                $element.addClass("non-focus");
+            } else {
+                $element.removeClass("non-focus");
+            }
+
+        });
+    };
+
+});
