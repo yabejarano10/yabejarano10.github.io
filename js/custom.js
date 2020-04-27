5f2a7fc1e663475b26e91760beaebe11db5ef8e0
Yesid  Bejarano <yabcyesid12@gmail.com>
2020-04-27 11:02:29 -0500

    Added projects

--- js/custom.js
+++ js/custom.js
@@ -104,7 +104,11 @@ $(document).ready(function () {
     });
 
     //animatedModal
-    $("#demo01,#demo02,#demo03,#demo04,#demo05,#demo06,#demo07,#demo08,#demo09").animatedModal();
+    $("#demo01,#demo05,#demo06,#demo07,#demo08,#demo09").animatedModal();
+    $("#demo02").animatedModal2();
+    $("#demo03").animatedModal3();
+    $("#demo04").animatedModal4();
+
 
     // Contact Form 	
 
